367d4b7e84d82d293ee88e288453735e0b28512c
Lisa <Lisa@Lisa-PC>
2023-08-10 15:43:58 +0530

    First commit

---
+++ ArtificialQspider/src/test/java/actitimeApp/GitHub_Practice.java
@@ -0,0 +1,12 @@
+package actitimeApp;
+
+public class GitHub_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("github");
+
+	}
+
+}
